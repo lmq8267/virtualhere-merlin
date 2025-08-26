185d218c51d2cbdda83a7e31197222f6649f308a
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2025-08-26 10:50:16 +0800

    Update vhusbd_config.sh

--- koolshare/virtualhere/scripts/vhusbd_config.sh
+++ koolshare/virtualhere/scripts/vhusbd_config.sh
@@ -128,7 +128,7 @@ EOF
         		sed -i '/^clientAuthorization=/d' /koolshare/configs/vhusbd/vhusbd.ini
     		fi
 	fi
-	TZ=UTC-8 /koolshare/bin/vhusbd -c /koolshare/configs/vhusbd/vhusbd.ini -b ${cmd}-r /tmp/upload/vhusbd.log &
+	TZ=UTC-8 /koolshare/bin/vhusbd -c /koolshare/configs/vhusbd/vhusbd.ini -b ${cmd} -r /tmp/upload/vhusbd.log &
 	sleep 5
 	if [ ! -z "$(pidof vhusbd)" ] ; then
 		logg "vhusbd-$ver 启动成功！"
